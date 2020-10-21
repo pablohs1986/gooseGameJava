299471e08f4fac73a02cf676dfcfff00acb80bef
pHerrero <pherrero@localhost>
2020-10-21 12:54:50 +0200

    Working on anadirJugadores()

--- src/main/java/modelo/Partida.java
+++ src/main/java/modelo/Partida.java
@@ -13,8 +13,17 @@ import java.util.Scanner;
  * @author Pablo Herrero
  */
 public class Partida {
-    Tablero t = new Tablero();
+    Tablero tablero = new Tablero();
     ArrayList<Jugador> jugadores = new ArrayList<>();
+    
+    public Partida() {
+    }
+    
+    public void iniciarPartida() {
+        anadirJugadores();
+        tablero.crearCasillas();
+        tablero.mostrarTablero();
+    }
     
     public void anadirJugadores() {
         Scanner sc = new Scanner(System.in);
@@ -25,23 +34,16 @@ public class Partida {
         System.out.println("Introducir el número de jugadores:");
         entradaNumeroJugadores = sc.nextInt();
         
-        for (int i = 1; i < entradaNumeroJugadores; i++) {
+        for (int i = 1; i <= entradaNumeroJugadores; i++) {
             System.out.println("Introduce el nombre de Jugador 1");
+            sc.next();
             entradaNombreJugadores = sc.nextLine();
-            jugadores.add(new Jugador(i, entradaNombreJugadores, t.getCasillasTablero().get(0), 0));
+            Casilla casillaInicio = tablero.getCasillasTablero(0);
+            jugadores.add(new Jugador(i, entradaNombreJugadores, casillaInicio, i)); // Seguir aquí
+//            jugadores.add(new Jugador(i, entradaNombreJugadores, tablero.getCasillasTablero(0), 0));
+//            System.out.println(jugadores.get(i).getNombre() + " añadido a la partida");
         }
         
-    }
-    
-    
-    public void iniciarPartida() {
-        
-
-        t.crearCasillas();
-        t.mostrarTablero();
-    }
-
-    public Partida() {
     }
     
 }
