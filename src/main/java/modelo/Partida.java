e9f2d65c5a64aa24f92549babc07201cc926a82c
pHerrero <pherrero@localhost>
2020-10-21 12:07:38 +0200

    Added ArrayList of Boxes and working on Game class and methods.

--- src/main/java/modelo/Partida.java
+++ src/main/java/modelo/Partida.java
@@ -5,10 +5,43 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Pablo Herrero
  */
 public class Partida {
+    Tablero t = new Tablero();
+    ArrayList<Jugador> jugadores = new ArrayList<>();
+    
+    public void anadirJugadores() {
+        Scanner sc = new Scanner(System.in);
+        int entradaNumeroJugadores;
+        String entradaNombreJugadores;
+        
+        
+        System.out.println("Introducir el número de jugadores:");
+        entradaNumeroJugadores = sc.nextInt();
+        
+        for (int i = 1; i < entradaNumeroJugadores; i++) {
+            System.out.println("Introduce el nombre de Jugador 1");
+            entradaNombreJugadores = sc.nextLine();
+            jugadores.add(new Jugador(i, entradaNombreJugadores, t.getCasillasTablero().get(0), 0));
+        }
+        
+    }
+    
+    
+    public void iniciarPartida() {
+        
+
+        t.crearCasillas();
+        t.mostrarTablero();
+    }
+
+    public Partida() {
+    }
     
 }
